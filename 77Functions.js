f8c89dd54f4831cb64683f67cec92469da1ff361
Mariela <mvmihilova@gmail.com>
2023-03-29 06:56:14 -0600

    updated

--- 77Functions.js
+++ 77Functions.js
@@ -1,3 +1,9 @@
 //Functions allow us to write reusable, modular code
 //a "CHUNK" of code is defined when it can be executed at a later point
+
 //SYNTAX:
+//function funcName(){ do something }
+
+//BUILT IN METHODS:
+//'hello'.toUpperCase
+//
