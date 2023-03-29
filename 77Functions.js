50403f9d5cbeaf8638eb71ad3f7905e3123cccec
Mariela <mvmihilova@gmail.com>
2023-03-29 06:56:25 -0600

    updated again

--- 77Functions.js
+++ 77Functions.js
@@ -4,6 +4,23 @@
 //SYNTAX:
 //function funcName(){ do something }
 
+//TO RUN THE FUNCTION:
+//funcName(); //run once
+//funcName(); //run again!
+
 //BUILT IN METHODS:
 //'hello'.toUpperCase
 //
+
+function grumpus() {
+  console.log("ohhhh, you again!");
+  console.log("LEAVE ME ALONE :)!");
+}
+grumpus();
+console.log("== You can call it second time: ===");
+grumpus();
+console.log();
+console.log("== You can run it in a loop: ===");
+for (let i = 0; i < 50; i++) {
+  grumpus();
+}
